99370ae3288454258023cbe72f238ecb8650a92e
Tusk98 <henryracoomb@gmail.com>
2020-04-25 15:19:21 -0400

    Completed transformation functions, working on fcn's for calling it

--- text_segmentation/transform.py
+++ text_segmentation/transform.py
@@ -9,13 +9,13 @@ import cv2
 def order_points(pts):
     rectangle = np.zeroes((4, 2),dtype="float32")
 
-    # the top-left point will have the smallest sum, whereas
+    # the top-left point will have the smallest x + y sum
     # the bottom-right point will have the largest sum
     s = pts.sum(axis=1)
     rectangle[0] = pts[np.argmin(s)]
     rectangle[2] = pts[np.argmax(s)]
 
-    # compute the difference between the points, the
+    # compute the difference (x - y) between the points
     # top-right point will have the smallest difference,
     # whereas the bottom-left will have the largest difference
     diff = np.diff(pts, axis = 1)
@@ -23,3 +23,33 @@ def order_points(pts):
     rectangle[3] = pts[np.argmax(diff)]
 
     return rectangle
+
+def four_point_transform(image, pts):
+    # Order the image coordinates using helper function
+    rectangle = order_points(pts)
+    (top_left, top_right, bottom_right, bottom_left) = order_points(pts)
+
+    # Determine width of new, transformed image using max distance between
+    # either top or bottom set of coordinates
+    widthBottom = np.sqrt(((bottom_right[0] - bottom_left[0]) ** 2) + ((bottom_right[1] - bottom_left[1]) ** 2))
+    widthTop = np.sqrt(((top_right[0] - top_left[0]) ** 2) + ((top_right[1] + top_right[1])**2))
+    new_width = max(int(widthBottom), int(widthTop))
+
+    # Determine new height using similar technique but with right and left instead
+    heightLeft = np.sqrt(((top_left[0] - bottom_left[0]) ** 2) + ((top_left[1] - bottom_left[1]) ** 2))
+    heightRight = np.sqrt(((top_right[0] - bottom_right[0]) ** 2) + ((top_right[1] - bottom_right[1]) ** 2))
+    new_height = max(int(heightLeft), int(heightRight))
+
+    # Define our top-down view (the coordinates of the corners of our new image
+    new_coords = np.array([
+        [0, 0],                             # Top left corner
+        [new_width - 1, 0],                 # Top right
+        [new_width - 1, new_height - 1],    # Bottom right
+        [0, new_height - 1]],               # Bottom left
+        dtype="float32")
+
+    # lol cv2 what is this even
+    Matrix = cv2.getPerspectiveTransform(rectangle, new_coords)
+    warped = cv2.warpPerspective(image, Matrix, (new_width, new_height))
+
+    return warped
